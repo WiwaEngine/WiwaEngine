3898de69ca2bcc313520fc8fa82d1da3a35298b5
Taks7 <albramisaplay@gmail.com>
2023-02-21 20:45:24 +0100

    Base Particle System

---
+++ Wiwa/src/Wiwa/ecs/systems/ParticleSystem.cpp
@@ -0,0 +1,36 @@
+#include <wipch.h>
+#include "ParticleSystem.h"
+
+namespace Wiwa {
+	ParticleSystem::ParticleSystem()
+	{
+
+	}
+
+	ParticleSystem::~ParticleSystem()
+	{
+
+	}
+
+	void ParticleSystem::OnAwake()
+	{
+		
+	}
+
+	void ParticleSystem::OnInit()
+	{
+		
+	}
+
+	void ParticleSystem::OnUpdate()
+	{
+		
+	}
+
+	void ParticleSystem::OnDestroy()
+	{
+		
+	}
+}
+
+
---
+++ Wiwa/src/Wiwa/ecs/systems/ParticleSystem.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "System.h"
+#include "../components/Transform3D.h"
+#include "../components/AudioSource.h"
+
+#include <vector>
+#include <map>
+
+#include <Wiwa/utilities/Reflection.h>
+
+namespace Wiwa {
+	class WI_API ParticleSystem : public System {
+	private:
+
+	public:
+		ParticleSystem();
+		~ParticleSystem();
+
+		void OnAwake() override;
+
+		void OnInit() override;
+
+		void OnUpdate() override;
+
+		void OnDestroy() override;
+	};
+}
+
+REGISTER_SYSTEM(Wiwa::ParticleSystem);
